cf2293877588792059d8071017bc71632fc99e98
CigdemOzel <cigdemozel17@gmail.com>
2025-03-08 17:45:40 +0300

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.example.entity.Task;
+import org.example.entity.TaskData;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Set<Task> annsTasks = new HashSet<>();
+        Set<Task> bobsTasks = new HashSet<>();
+        Set<Task> carolsTasks = new HashSet<>();
+        Set<Task> unassignedTasks = new HashSet<>();
+        TaskData taskData = new TaskData(annsTasks, bobsTasks, carolsTasks,unassignedTasks);
+
     }
 }
